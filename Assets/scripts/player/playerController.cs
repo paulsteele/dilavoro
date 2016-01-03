ed77fe6a588c5999c15bb67c05d739648d982b03
Paul Steele <paul_steele@live.com>
2016-01-03 14:37:55 -0500

    added preliminary movement for player

---
+++ Assets/scripts/player/playerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+    public KeyCode keyLeft;
+    public KeyCode keyRight;
+    public int horizontalMoveSpeed;
+    private Rigidbody2D body;
+    private Vector2 vector;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+	}
+	
+	// FixedUpdate is called once per frame
+	void FixedUpdate () {
+	    if (Input.GetKey(keyLeft)) {
+            vector = new Vector2(-1.0f, 0.0f) * horizontalMoveSpeed;
+            body.AddForce(vector);
+        }
+
+        if (Input.GetKey(keyRight))
+        {
+            vector = new Vector2(1.0f, 0.0f) * horizontalMoveSpeed;
+            body.AddForce(vector);
+        }
+	}
+}
