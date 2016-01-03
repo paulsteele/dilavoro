3c2a7db4edcfc2c3f9ddcec7c4271df9f41cf0bc
Paul Steele <paul_steele@live.com>
2016-01-03 18:51:51 -0500

    jumping can only be done on ground

--- Assets/scripts/player/playerController.cs
+++ Assets/scripts/player/playerController.cs
@@ -9,14 +9,21 @@ public class playerController : MonoBehaviour {
     public int horizontalMoveSpeed;
     public int jumpSpeed;
 
-
     private Rigidbody2D body;
     private Vector2 vector;
+    private float width;
+    private float height;
 
 	// Use this for initialization
 	void Start () {
         body = GetComponent<Rigidbody2D>();
-	}
+        //get height and width for purposes of collision detection
+        Sprite sp = GetComponent<SpriteRenderer>().sprite;
+        width = (sp.bounds.max.x - sp.bounds.min.x) / 2;
+        height = (sp.bounds.max.y - sp.bounds.min.y) / 2;
+        width *= body.transform.localScale.x;
+        height *= body.transform.localScale.y;
+    }
 	
 	// FixedUpdate is called once per timestep
 	void FixedUpdate () {
@@ -34,8 +41,15 @@ public class playerController : MonoBehaviour {
             deltaSpeedX += horizontalMoveSpeed;
         }
         //jump
-        if (Input.GetKeyDown(keyUp)) {
-            deltaSpeedY += jumpSpeed;
+        if (Input.GetKey(keyUp)) {
+            
+            Vector2 topLeft = new Vector2(body.position.x - width, body.position.y - height);
+            Vector2 bottomRight = new Vector2(body.position.x + width, body.position.y - height -1 );
+            bool grounded = Physics2D.OverlapArea(topLeft, bottomRight, LayerMask.NameToLayer("StaticTerrain"));
+            if (grounded && deltaSpeedY.CompareTo(0.0f) == 0) {
+                deltaSpeedY += jumpSpeed;
+            }
+            
         }
 
         //apply movement
