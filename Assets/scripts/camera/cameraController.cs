d19730bbc81bb306b58799f3d4d3d09b48dca721
Paul Steele <paul_steele@live.com>
2016-01-04 18:59:00 -0500

    begain basics of camera following

---
+++ Assets/scripts/camera/cameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraController : MonoBehaviour {
+
+    public GameObject focus;
+    public float horizontalPadding;
+    private float width;
+    private float centerx;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
