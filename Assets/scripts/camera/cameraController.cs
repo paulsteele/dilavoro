c051448b6b211f8be47cc19548dd5c09b802a630
Paul Steele <paul_steele@live.com>
2016-01-05 20:40:46 -0500

    added scrolling camera

--- Assets/scripts/camera/cameraController.cs
+++ Assets/scripts/camera/cameraController.cs
@@ -3,17 +3,37 @@ using System.Collections;
 
 public class cameraController : MonoBehaviour {
 
-    public GameObject focus;
+    public string focustext;
     public float horizontalPadding;
-    private float width;
+    private GameObject focus;
+    private float radius;
     private float centerx;
 	// Use this for initialization
 	void Start () {
-	    
-	}
+        radius = Camera.main.orthographicSize * Screen.width / Screen.height;
+        focus = GameObject.Find(focustext);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        //bounds check
+        float difference = focus.transform.position.x - transform.position.x;
+        if (Mathf.Abs(difference).CompareTo(horizontalPadding) > 0) {
+            int comp = difference.CompareTo(0);
+            Vector3 move;
+            if (comp > 0) {
+                move = new Vector3(focus.transform.position.x - horizontalPadding, transform.position.y, transform.position.z);
+
+            }
+            else if (comp < 0) {
+                move = new Vector3(focus.transform.position.x + horizontalPadding, transform.position.y, transform.position.z);
+            }
+            else {
+                return;
+            }
+            transform.position = move;
+        }
+        
+
+    }
 }
