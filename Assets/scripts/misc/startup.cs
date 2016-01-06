88f309b53c0e3d9c0b5f48af10b59600bc6a66bd
Paul Steele <paul_steele@live.com>
2016-01-06 13:09:00 -0500

    added startup script, added ability to reset prefs so game not locked in ugly 4:3 res

---
+++ Assets/scripts/misc/startup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startup : MonoBehaviour {
+
+    public bool resetPrefs;
+	//faster startup
+	void Start() {
+        
+        //clear prefs
+        if (resetPrefs) {
+            PlayerPrefs.DeleteAll();
+            Debug.Log("Prefs reset");
+            Application.Quit();
+        }
+    }
+}
