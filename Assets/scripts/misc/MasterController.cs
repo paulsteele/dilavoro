d2b56b24fb6701ce8f0ca1e7e88a4bf11d319f0d
Paul Steele <paul_steele@live.com>
2016-01-08 14:07:09 -0500

    beat engine 60bpm implementation

--- Assets/scripts/misc/MasterController.cs
+++ Assets/scripts/misc/MasterController.cs
@@ -1,10 +1,15 @@
-﻿using UnityEngine;
+﻿using UnityEngine.UI;
+using UnityEngine;
 using System.Collections;
 
 public class MasterController : MonoBehaviour {
 
     public bool resetPrefs;
-	
+    public int bpm;
+    private int currentBeat;
+    private int partialBeat;
+    private int partialMax;
+    private Text bpmText;
     //faster startup
 	void Start() { 
         //clear prefs
@@ -13,9 +18,27 @@ public class MasterController : MonoBehaviour {
             Debug.Log("Prefs reset");
             Application.Quit();
         }
+        //setup bpm counters
+        currentBeat = 0;
+        partialBeat = 0;
+        partialMax = ( 60) * (int) (1 / Time.fixedDeltaTime);
+        bpmText = GameObject.Find("bpmcounter").GetComponent<Text>();
     }
 
     public void onHit() {
         Debug.Log("player hit");
+    }
+
+    void FixedUpdate() {
+        partialBeat += bpm;
+        //bpmText.text = "" + partialBeat + " / " + partialMax + " BPM";
+        if (partialBeat == partialMax) {
+            partialBeat = 0;
+            currentBeat++;
+            if (currentBeat > bpm) {
+                currentBeat = 0;
+            }
+            bpmText.text = "" + currentBeat + " / " + bpm + " BPM";
+        }
     }
 }
