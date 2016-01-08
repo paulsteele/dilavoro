0d1bb4444c8867af9ee13b583198f03522fcfeee
Paul Steele <paul_steele@live.com>
2016-01-08 15:25:58 -0500

    changed fixed step to 60fps, giving more bpm choices

--- Assets/scripts/misc/MasterController.cs
+++ Assets/scripts/misc/MasterController.cs
@@ -23,6 +23,7 @@ public class MasterController : MonoBehaviour {
         currentBeat = 0;
         partialBeat = 0;
         partialMax = ( 60) * (int) (1 / Time.fixedDeltaTime); //typically will be 3000, but this is the "bar" the bpm has to fill inorder to be on the next beat
+        Debug.Log(partialMax);
         bpmText = GameObject.Find("bpmcounter").GetComponent<Text>();
     }
 
@@ -33,7 +34,9 @@ public class MasterController : MonoBehaviour {
     void FixedUpdate() {
         partialBeat += bpm; //add the bpm to the bar to fill
         if (partialBeat >= partialMax) { //if filled or overfilled 
+            
             partialBeat = partialBeat - partialMax; //set the partial to the extra left over so eventually beat get back in sync
+            Debug.Log(partialBeat);
             currentBeat++;
             AudioSource.PlayClipAtPoint(audio, Camera.main.transform.position);
             if (currentBeat > bpm) {
